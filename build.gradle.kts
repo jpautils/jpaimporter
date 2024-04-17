9835a1a073021e5c360ed505d72c19da9e70711d
a13a <5953798+a13a@users.noreply.github.com>
2024-04-17 11:20:20 +0100

    Adding gradle build tool.

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("java")
+    id("groovy")
+}
+
+group = "io.github.jpautils"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
+    implementation("org.slf4j:slf4j-api:2.0.13")
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    testImplementation(platform("org.apache.groovy:groovy-bom:4.0.20"))
+    testImplementation("org.apache.groovy:groovy")
+    testImplementation(platform("org.spockframework:spock-bom:2.3-groovy-4.0"))
+    testImplementation("org.spockframework:spock-core")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
